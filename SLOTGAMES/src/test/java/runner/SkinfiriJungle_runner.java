c456d5832e9fbecbd2f1d7c998e1b6f79573e352
pavanysecit <pavan.kumar@ysecit.com>
2020-04-15 10:47:38 +0530

    .

---
+++ SLOTGAMES/src/test/java/runner/SkinfiriJungle_runner.java
@@ -0,0 +1,13 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:/Users/sw176/git/SLOTGAMES_MOBILES/SLOTGAMES/features/SkinfiriJungle_AllUsecases.feature", glue={"stepDefinition_SkinfiriJungle"})
+
+
+public class SkinfiriJungle_runner {
+
+}
