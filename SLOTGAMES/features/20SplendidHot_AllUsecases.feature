5941c5ac153e3a92e626dd9ddded2c280c8b1bdf
pavanysecit <pavan.kumar@ysecit.com>
2020-07-01 13:01:54 +0530

    July 1st

---
+++ SLOTGAMES/features/20SplendidHot_AllUsecases.feature
@@ -0,0 +1,7 @@
+Feature: Test all the functionalities of 20 Splendid Hot Slot Game
+   
+
+		Scenario: Select the bet type as 0.02 from the drop down and check the bet value should be 0.4,0.8,2,4 & 8 in 20 Splendid Hot slot game
+    Given Chrome browser, valid URL, valid login details, 20 Splendid Hot game, bet type as 0.02 and bet value as 0.4,0.8,2,4 & 8
+    When Open the 20 Splendid Hot slot game by entering the valid URL in browser, enter the valid login details, select the bet type as 0.02 and check the bet value should be 0.4,0.8,2,4 & 8
+    Then Bet value should get changed to 0.4,0.8,2,4 & 8 if the bet type selected as 0.02 from the drop down 1 in 20 Splendid Hot slot game
---
+++ SLOTGAMES/src/test/java/stepDefinition_20SplendidHot/TwentySplendidHot_URL_Login.java
@@ -0,0 +1,65 @@
+package stepDefinition_20SplendidHot;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.sikuli.script.FindFailed;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class TwentySplendidHot_URL_Login {
+private static AppiumDriver<MobileElement> driver;
+	
+	//@Before
+	public static void  TwentySplendidHot_URL_Login() throws InterruptedException, MalformedURLException, FindFailed {
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability("deviceName", "ASUS X00TD");
+		cap.setCapability("udid", "J9AAGF10J33379E");
+		cap.setCapability("platformName", "Android");
+		cap.setCapability("platformVersion", "9");
+		cap.setCapability("browserName", "Chrome");
+		
+		URL url=new URL("http://10.10.13.84:4723/wd/hub");
+		
+		driver=new AndroidDriver<MobileElement>(url,cap);
+		
+		System.out.println("Appium started sucessfully");
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		
+		driver.get("http://demo.ysecit.in:82/SlotGames/slotsgame");
+		Thread.sleep(3000);
+
+		driver.findElement(By.xpath("/html/body/div[3]/div[1]/ui-view/section/section[1]/div/div/div[2]/div[3]/ul/li[1]/div[1]/div")).click();
+		Thread.sleep(2000);
+				 
+		driver.findElement(By.name("email")).sendKeys("8017627028");
+		Thread.sleep(2000);
+				
+		driver.findElement(By.xpath("/html/body/div[3]/header/section/div[9]/div/div[2]/form/div[2]/input")).sendKeys("mans@123");
+		Thread.sleep(2000);
+		
+		MobileElement sub = driver.findElement(By.xpath("/html/body/div[3]/header/section/div[9]/div/div[2]/form/div[3]"));
+		JavascriptExecutor jse=(JavascriptExecutor)driver;
+		jse.executeScript("arguments[0].click();", sub);
+		Thread.sleep(5000);
+		
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		WebElement  login_button=  driver.findElement(By.xpath("/html/body/div[3]/div[1]/ui-view/section/section[1]/div/div/div[2]/div[3]/ul/li[1]/div[1]/div"));
+        wait.until(ExpectedConditions.elementToBeClickable(login_button));
+        login_button.click();
+        
+        Thread.sleep(5000);
+	}
+	
+	public static AppiumDriver<MobileElement> getDriver()
+	{
+		return driver;
+	}
+}
