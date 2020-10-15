e8cee8701e1174f0b974f468f977c02fa0ed12db
pavanysecit <pavan.kumar@ysecit.com>
2020-10-15 11:39:28 +0530

    Happy Fruits

---
+++ SLOTGAMES/src/test/java/stepDefinition_HappyFruits/HappyFruits_AutoSpin_AutoSpins.java
@@ -0,0 +1,88 @@
+package stepDefinition_HappyFruits;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class HappyFruits_AutoSpin_AutoSpins {
+AppiumDriver<MobileElement> driver;
+
+	public HappyFruits_AutoSpin_AutoSpins() throws InterruptedException {
+		this.driver = HappyFruits_URL_Login.getDriver();
+		//this.driver = HappyFruits_URL_TryNow.getDriver();
+	}
+	
+	@Given("^Chrome browser, valid URL, valid login details, Happy Fruits slot game, balance, spin button, auto spin button, auto spins option and number of spins left message$")
+	public void chrome_browser_valid_URL_valid_login_details_Happy_Fruits_slot_game_balance_spin_button_auto_spin_button_auto_spins_option_and_number_of_spins_left_message() throws Throwable {
+		WebDriverWait wait1 = new WebDriverWait(driver, 80);
+		wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("hud_Hud_txtBalance1"))); 
+	}
+
+	@When("^Open the Happy Fruits slot game by entering the valid URL in browser, enter the valid login details, transfer the balance, click on auto spin option under auto spin drop down and check the player instruction message$")
+	public void open_the_Happy_Fruits_slot_game_by_entering_the_valid_URL_in_browser_enter_the_valid_login_details_transfer_the_balance_click_on_auto_spin_option_under_auto_spin_drop_down_and_check_the_player_instruction_message() throws Throwable {
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("hud_btnAuto")));
+		driver.findElement(By.id("hud_btnAuto")).click();
+		Thread.sleep(3000);
+
+		System.out.println("Auplay Auto: Started spinning the real : ");
+		
+		
+		wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("hud_txtWinDetail"), "GOOD LUCK!"));
+	    String actual = driver.findElement(By.id("hud_txtWinDetail")).getText();
+	    String expected = "GOOD LUCK!";
+	    System.out.println(actual);
+	    Assert.assertEquals(actual, expected);
+	    Thread.sleep(2000);
+ 
+	    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("hud_txtWinDetail"), "GOOD LUCK!"));
+	    String actual1 = driver.findElement(By.id("hud_txtWinDetail")).getText();
+	    String expected1 = "GOOD LUCK!";
+	    System.out.println(actual1);
+	    Assert.assertEquals(actual1, expected1);
+	    Thread.sleep(2000);
+	    
+	    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("hud_txtWinDetail"), "GOOD LUCK!"));
+	    String actual2 = driver.findElement(By.id("hud_txtWinDetail")).getText();
+	    String expected2 = "GOOD LUCK!";
+	    System.out.println(actual2);
+	    Assert.assertEquals(actual2, expected2);
+	    Thread.sleep(2000);
+	    
+	    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("hud_txtWinDetail"), "GOOD LUCK!"));
+	    String actual3 = driver.findElement(By.id("hud_txtWinDetail")).getText();
+	    String expected3 = "GOOD LUCK!";
+	    System.out.println(actual3);
+	    Assert.assertEquals(actual3, expected3);
+	    Thread.sleep(2000);
+	    
+	    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("hud_txtWinDetail"), "GOOD LUCK!"));
+	    String actual4 = driver.findElement(By.id("hud_txtWinDetail")).getText();
+	    String expected4 = "GOOD LUCK!";
+	    System.out.println(actual4);
+	    Assert.assertEquals(actual4, expected4);
+	    Thread.sleep(2000);
+	    
+	    driver.findElement(By.id("hud_btnSpin")).click();
+	    Thread.sleep(5000);
+	    
+	    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("hud_txtWinDetail"), "Please Place Your Bet"));
+	    String actual5 = driver.findElement(By.id("hud_txtWinDetail")).getText();
+	    String expected5 = "Please Place Your Bet";
+	    System.out.println(actual5);
+	    Assert.assertEquals(actual5, expected5);
+	}
+
+	@Then("^System should keep performing the spins in Happy Fruits slot game$")
+	public void system_should_keep_performing_the_spins_in_Happy_Fruits_slot_game() throws Throwable {
+	    Thread.sleep(2000);
+	    driver.quit();
+	}
+}
---
+++ SLOTGAMES/src/test/java/stepDefinition_HappyFruits/HappyFruits_Balance_Deduction_AccordingToBetType2_5.java
@@ -0,0 +1,79 @@
+package stepDefinition_HappyFruits;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class HappyFruits_Balance_Deduction_AccordingToBetType2_5 {
+	AppiumDriver<MobileElement> driver;
+
+	public HappyFruits_Balance_Deduction_AccordingToBetType2_5() throws InterruptedException {
+		this.driver = HappyFruits_URL_Login.getDriver();
+		//this.driver = HappyFruits_URL_TryNow.getDriver();
+	}
+	
+	@Given("^Chrome browser, valid URL, valid login details, Happy Fruits game, bet type as (\\d+)\\.(\\d+), bet value as FIVE, bet value drop down, balance and spin button$")
+	public void chrome_browser_valid_URL_valid_login_details_Happy_Fruits_game_bet_type_as_bet_value_as_FIVE_bet_value_drop_down_balance_and_spin_button(int arg1, int arg2) throws Throwable {
+		WebDriverWait wait1 = new WebDriverWait(driver, 80);
+		wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("hud_Hud_txtBalance1"))); 
+	}
+
+	@When("^Open the Happy Fruits slot game by entering the valid URL in browser, enter the valid login details, select the bet type as (\\d+)\\.(\\d+), select the bet value to FIVE, click on spin button and check the Happy Fruits slot game balance$")
+	public void open_the_Happy_Fruits_slot_game_by_entering_the_valid_URL_in_browser_enter_the_valid_login_details_select_the_bet_type_as_select_the_bet_value_to_FIVE_click_on_spin_button_and_check_the_Happy_Fruits_slot_game_balance(int arg1, int arg2) throws Throwable {
+		//Storing the value before spin
+		String preSpin = driver.findElement(By.id("hud_Hud_txtBalance1")).getText();
+		String str = preSpin.replaceAll(",", "");
+		System.out.println("Current balance of the account Before spin: " +str);
+		
+		//Selecting the credit as 0.05 from the drop down
+		driver.findElement(By.id("hud_txtCredit")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("hud_CreditPopup20.05")).click();
+		Thread.sleep(2000);
+		
+		String actual = driver.findElement(By.id("hud_txtCredit")).getText();
+		System.out.println("Selected credit value is : " +actual);
+		String expected = "0.05";
+		Assert.assertEquals(actual, expected);
+		
+		//Selecting the bet amount as 5 from the drop down
+		driver.findElement(By.id("hud_txtBetAmount")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("hud_BetPopup55")).click();
+		Thread.sleep(2000);
+		
+		String actual1 = driver.findElement(By.id("hud_txtBetAmount")).getText();
+		String expected1 = "5";
+		System.out.println("Selected bet value is : " +actual1);
+		Assert.assertEquals(actual1, expected1);
+			
+		//Clicking on Spin button
+		driver.findElement(By.id("hud_btnSpin")).click();
+		Thread.sleep(5000);
+		
+		//Storing the value after spin
+		String postSpin = driver.findElement(By.id("hud_Hud_txtBalance1")).getText();
+		String str1 = postSpin.replaceAll(",", "");
+		System.out.println("Current balance of the account After spin: " +str1);
+		
+		//Deducting bet value from the preSpin and formating string to double
+		double fValue = Double.parseDouble(str) - Double.parseDouble(actual1);
+		String dbi = String.format("%.2f", fValue);  
+		System.out.println("Final balance after deducting bet amount from the balance : "+dbi);
+		
+		Assert.assertEquals(dbi,str1);
+	}
+
+	@Then("^Existing Balance should get deducted by FIVE as bet type is selected as (\\d+)\\.(\\d+) & bet value as FIVE in Happy Fruits slot game$")
+	public void existing_Balance_should_get_deducted_by_FIVE_as_bet_type_is_selected_as_bet_value_as_FIVE_in_Happy_Fruits_slot_game(int arg1, int arg2) throws Throwable {
+	    Thread.sleep(2000);
+	    driver.quit();
+	}
+}
